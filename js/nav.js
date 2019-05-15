a88c9097293584905c520babc446ab577e9eb209
Curtis Adams <curtisadams28@gmail.com>
2019-05-15 16:31:56 +1200

    Fixed default screen and tapu-aa-nuku page

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,4 @@
 
-var activestarmode;
 $.ajax({
   url: "https://matarikiwaka-functions.azurewebsites.net/api/waka/current",
 }).done(function(data) {
@@ -7,87 +6,89 @@ $.ajax({
     var isactive = data.starModes[i].isActive;
     var starmode = data.starModes[i].starMode;
     if (isactive == true) {
-      activestarmode = starmode;
+      break;
     }
   }
-  test = data.starModes;
-  //console.log(test);
-  console.log("inside function: " + activestarmode);
-  return activestarmode;
+  console.log(starmode);
+  doAction(starmode);
+  //return activestarmode;
 });
-console.log();
 
-var stardesc = ["Eyes of God", "Salt Water", "Fresh Water", "That which grows in the sky", "That which grows in the ground", "Wind and weather", "Water that pools in the sky"];
+
+var stardesc = ["Eyes of God", "Salt Water", "Fresh Water",
+"That which grows in the sky", "That which grows in the ground",
+"Wind and weather", "Water that pools in the sky"];
+
 $(document).ready(function() {
 
-  var starmode = "Waipunaaarangi";
-/*
-  switch (starmode) {
-    case "Matariki": defaultScreen(starmode, 0);
-    case "Waitaa": defaultScreen(starmode, 1);
-    case "Waitii": defaultScreen(starmode, 2);
-    case "Tapu-aa-rangi": defaultScreen(starmode, 3);
-    case "Tapu-aa-nuku": defaultScreen(starmode, 4);
-    case "Ururangi": defaultScreen(starmode, 5);
-    case "Waipuna-aa-rangi": defaultScreen(starmode, 6);
-    break;
-  }
-  */
-
-  if (starmode == "Matariki") {
-      defaultScreen(starmode, 0);
-  }
-  if (starmode == "Waitaa") {
-      defaultScreen(starmode, 1);
-  }
-  if (starmode == "Waitii") {
-      defaultScreen(starmode, 2);
-  }
-  if (starmode == "Tupuaarangi") {
-      defaultScreen("Tapu-aa-rangi", 3);
-  }
-  if (starmode == "Tupuaanuku") {
-      defaultScreen("Tapu-aa-nuku", 4);
-  }
-  if (starmode == "Ururangi") {
-      defaultScreen(starmode, 5);
-  }
-  if (starmode == "Waipunaaarangi") {
-      defaultScreen("Waipuna-aa-rangi", 6);
-  }
-
-  function defaultScreen(starmode, i) {
-
-    console.log(i);
-
-    $('#content').load('html/' + starmode + '.html');
-    document.getElementById("title").innerHTML = starmode;
-    document.getElementById("desc").innerHTML = stardesc[i];
-    changeBackground(starmode);
-  }
-
-  $('#links a').click(function() {
-    var url = $(this).attr('href');
-    urlsplit = url.split('&');
-    var page = urlsplit[0];
-    var desc = urlsplit[1];
-    //alert(urlsplit);
-
-    $('#content').load('html/' + page + '.html');
-
-    document.getElementById("title").innerHTML = page;
-    document.getElementById("desc").innerHTML = desc;
-
-    changeBackground(page);
-
-    urlsplit = [];
-
-    return false;
-
-
-
-  });
 });
+
+// add by Andy
+function doAction(starmode){
+  /*
+    switch (starmode) {
+      case "Matariki": defaultScreen(starmode, 0);
+      case "Waitaa": defaultScreen(starmode, 1);
+      case "Waitii": defaultScreen(starmode, 2);
+      case "Tapu-aa-rangi": defaultScreen(starmode, 3);
+      case "Tapu-aa-nuku": defaultScreen(starmode, 4);
+      case "Ururangi": defaultScreen(starmode, 5);
+      case "Waipuna-aa-rangi": defaultScreen(starmode, 6);
+      break;
+    }
+    */
+
+    if (starmode == "Matariki") {
+        defaultScreen(starmode, 0);
+    }
+    if (starmode == "Waitaa") {
+        defaultScreen(starmode, 1);
+    }
+    if (starmode == "Waitii") {
+        defaultScreen(starmode, 2);
+    }
+    if (starmode == "Tupuaarangi") {
+        defaultScreen("Tapu-aa-rangi", 3);
+    }
+    if (starmode == "Tupuaanuku") {
+        defaultScreen("Tapu-aa-nuku", 4);
+    }
+    if (starmode == "Ururangi") {
+        defaultScreen(starmode, 5);
+    }
+    if (starmode == "Waipunaaarangi") {
+        defaultScreen("Waipuna-aa-rangi", 6);
+    }
+
+    function defaultScreen(starmode, i) {
+
+      console.log(i);
+
+      $('#content').load('html/' + starmode + '.html');
+      document.getElementById("title").innerHTML = starmode;
+      document.getElementById("desc").innerHTML = stardesc[i];
+      changeBackground(starmode);
+    }
+
+    $('#links a').click(function() {
+      var url = $(this).attr('href');
+      urlsplit = url.split('&');
+      var page = urlsplit[0];
+      var desc = urlsplit[1];
+      //alert(urlsplit);
+
+      $('#content').load('html/' + page + '.html');
+
+      document.getElementById("title").innerHTML = page;
+      document.getElementById("desc").innerHTML = desc;
+
+      changeBackground(page);
+
+      urlsplit = [];
+
+      return false;
+    });
+}
 
 function changeBackground(page) {
   if (page == "Matariki"){
